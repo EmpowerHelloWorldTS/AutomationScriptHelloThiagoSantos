eb80bc4aec753c8475387c8ab568420ad01c210d
tsantosleal <thiagosantos.leal@hotmail.com>
2023-05-30 15:54:41 -0300

    Add project files.

---
+++ EmpowerHelloWorld_TS_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerHelloWorld_TS_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerHelloWorld_TS_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("9333DCD3-DE77-451F-AFCC-5C6C0658CF03")]
+[assembly: AssemblyVersion("1.0.0.0")]
